0e452d97fe044c2b7a15a54dcb816cad431a54de
Colby Sato <cdjsato@gmail.com>
2016-06-26 08:40:37 -0400

    You can now edit items in the list, using itemsAdapter.insert and items.remove

--- app/src/main/java/com/example/colby/todolist/MainActivity.java
+++ app/src/main/java/com/example/colby/todolist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.colby.todolist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
 
@@ -26,15 +28,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lvItems = (ListView)findViewById(R.id.lvItems);
-        items = new ArrayList<>();
         readItems();
         itemsAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-        items.add("First Item");
-        items.add("Second Item");
+        //items.add("Wash Dishes");
+        //items.add("Make Dinner");
         setupListViewListener();
     }
+
+
 
     private void setupListViewListener() {
         lvItems.setOnItemLongClickListener(
@@ -48,7 +51,28 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        lvItems.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapter,View item, int pos, long id) {
+                        String selectedFromList = (lvItems.getItemAtPosition(pos)).toString(); // get the task in the list
+                        Intent i = new Intent(MainActivity.this, editItemActivity.class);
+                        // put "extras" into the bundle for access in the second activity
+                        i.putExtra("taskName", selectedFromList);
+                        // call getText().toString() on id?
+                        i.putExtra("position", pos);
+                        i.putExtra("mode", 2); // pass arbitrary data to launched activity
+                        // brings up the second activity
+                        startActivityForResult(i, REQUEST_CODE);
+                    }
+                    //whatt? How to receive data and update?
+
+                }
+        );
+        //is this how I get it to listen to a long click?
     }
+
 
     private void readItems() {
         File filesDir = getFilesDir();
@@ -73,11 +97,38 @@ public class MainActivity extends AppCompatActivity {
     public void onAddItem(View v) {
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
-        itemsAdapter.add(itemText);
+        int position = itemsAdapter.getCount();
+        itemsAdapter.insert(itemText, position);
         etNewItem.setText("");
         writeItems();
     }
 
+    private final int REQUEST_CODE = 20;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // REQUEST_CODE is defined above
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+
+            // Extract name value from result extras
+            //ListView etNewItem = (ListView) findViewById(R.id.etNewItem);
+
+            readItems();
+            writeItems();
+
+            String name = data.getExtras().getString("taskName");
+            int position = data.getExtras().getInt("position");
+            int code = data.getExtras().getInt("code", 0);
+            // Toast the name to display temporarily on screen
+            itemsAdapter.insert(name, position);
+            items.remove(position);
+            itemsAdapter.notifyDataSetChanged();
+
+            //Toast.makeText(this, name, Toast.LENGTH_SHORT).show(); // show the data passed back
+            writeItems();
+
+        }
+    }
 
 
 }
---
+++ app/src/main/java/com/example/colby/todolist/editItemActivity.java
@@ -0,0 +1,66 @@
+package com.example.colby.todolist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class editItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+        String taskName = getIntent().getStringExtra("taskName");
+        int position = getIntent().getIntExtra("position", 0);
+        int code = getIntent().getIntExtra("code", 0);
+
+        //write text. set default value, or write to the field?
+        EditText etNewItem = (EditText)findViewById(R.id.editText);
+        etNewItem.setText(taskName);
+        etNewItem.setSelection(taskName.length());
+
+
+        //etNewItem= taskName;
+
+//        EditText etNewItem = (EditText) findViewById(R.id.editText);
+//        etNewItem.add(taskName);
+//        etNewItem.setText("");
+//        writeItems();
+    }
+
+    public void saveChanges(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.editText);
+        String updatedItem = etNewItem.getText().toString();
+        // logcat.print(updatedItem)
+//        itemsAdapter.add(itemText);
+//        etNewItem.setText("");
+//        writeItems();
+        Intent data = new Intent();
+        // Pass relevant data back as a result
+        data.putExtra("taskName", etNewItem.getText().toString());
+        data.putExtra("position", 0);
+        data.putExtra("code", 20); // ints work too
+        // Activity finished ok, return the data
+        setResult(RESULT_OK, data); // set re
+        this.finish();
+        //when submit button is clicked, send data back
+    }
+
+//    public void onSubmit(View v) {
+//        //EditText etName = (EditText) findViewById(R.id.name);
+//        // Prepare data intent
+//        Intent data = new Intent();
+//        // Pass relevant data back as a result
+//        data.putExtra("name", "name");
+//        data.putExtra("code", 200); // ints work too
+//        // Activity finished ok, return the data
+//        setResult(RESULT_OK, data); // set result code and bundle data for response
+//        finish(); // closes the activity, pass data to parent
+//    }
+
+
+
+}
